2f134c36104a1d715e09785e8a050782b2d7ad2f
ufooo1998 <yurnero1681998@gmail.com>
2018-11-14 20:54:14 +0700

    Initial commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Authentication.Areas.Identity.Data;
+using Authentication.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Authentication.Areas.Identity.IdentityHostingStartup))]
+namespace Authentication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthenticationContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthenticationContextConnection")));
+
+                services.AddDefaultIdentity<AuthenticationUser>()
+                    .AddEntityFrameworkStores<AuthenticationContext>();
+            });
+        }
+    }
+}
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Authentication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authentication.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly AuthenticationContext _context;
+        public AdminController(AuthenticationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            
+
+            return View(_context.Users.ToList());
+        }
+
+        public IActionResult Roles()
+        {
+            return View(_context.Roles.ToList());
+        }
+
+        public IActionResult CreateRole()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateRole(IdentityRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Roles.Add(role);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Roles));
+            }
+            return View(role);
+        }
+
+        public async Task<IActionResult> EditRole(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _context.Roles.FindAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View(role);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditRole(string id, IdentityRole role)
+        {
+            if (id != role.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(role);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Roles));
+            }
+            return View(role);
+        }
+
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _context.Roles.FindAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("DeleteRole")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+            _context.Roles.Remove(role);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Roles));
+        }
+    }
+}
